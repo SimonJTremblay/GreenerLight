69257d2b6eddc148fea8729964604b6accd8d679
SimonJTremblay <stremblay@coach.ca>
2019-11-12 08:47:43 -0500

    Initial Commit

---
+++ src/WelcomeMessage/WelcomeMEssage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const WelcomeMessage = () =>{
+    return(
+        <div>
+            <div className='white f3'>
+                {'Hi Simon, how will we change the world today?'}
+            </div>
+
+           {/* { <div className='white f1'>
+                {'#5'}
+            </div>} */}
+        </div>
+    );
+}
+
+export default WelcomeMessage;
